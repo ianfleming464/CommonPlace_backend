a4c535db3a1122dccc05dc51141d8390a2baa9d6
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-03-27 23:56:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB database
-mongoose.connect(process.env.MONGODB_URI, {
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully');
+// });
+// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Connect locally
+mongoose.connect('mongodb://localhost:27017/CommonPlace', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Define API routes
 app.use('/api/notes', noteRoutes);
