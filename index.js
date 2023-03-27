6f0c23eb097ba916055aad07190e633090034319
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-03-27 21:58:49 +0200

    Added mongo connection, .env, notes routes

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB database
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-// });
-// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Define API routes
 app.use('/api/notes', noteRoutes);
---
+++ routes/notes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const noteController = require('../controllers/noteController');
+
+const router = express.Router();
+
+router.get('/', noteController.getAllNotes);
+router.get('/:id', noteController.getNoteById);
+router.post('/', noteController.createNote);
+router.put('/:id', noteController.updateNote);
+router.delete('/:id', noteController.deleteNote);
+
+module.exports = router;
