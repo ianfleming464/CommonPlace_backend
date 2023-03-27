5b0a53125d983179b2a5721e29378703be6de7d7
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-03-27 22:29:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Define API routes
